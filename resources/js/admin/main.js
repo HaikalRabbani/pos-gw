efa892dcec9fddf2f8a43bb1156062e91b3312db
HaikalRabbani <muhaikalrabbani@gmail.com>
2026-07-15 10:25:41 +0700

    feat: configure PrimeVue theme with custom preset and options

--- resources/js/admin/main.js
+++ resources/js/admin/main.js
@@ -3,9 +3,17 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import router from './router'
 import App from './App.vue'
+import MyPreset from './theme/preset'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      darkModeSelector: false,
+    },
+  },
+})
 app.mount('#app')
